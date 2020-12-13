3eb502750b0b8adfb81b7761b034c3360a56f1fe
Edgar Nova <ragnarok540@gmail.com>
2020-12-13 11:17:07 -0500

    project start

---
+++ demo/src/main/java/com/example/demo/service/HolaMundoService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+public interface HolaMundoService {
+
+	public void holaMundo();
+	
+}
---
+++ demo/src/main/java/com/example/demo/service/HolaMundoServiceBasic.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HolaMundoServiceBasic implements HolaMundoService {
+
+	@Override
+	public void holaMundo() {
+		System.out.print("Hola Mundo");
+		
+	}
+
+}
